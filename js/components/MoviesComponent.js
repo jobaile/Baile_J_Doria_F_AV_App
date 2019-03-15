1801c81ec003bf8563026482ff06f4d00c955ff5
nandodoria <nandodoria@hotmail.ca>
2019-03-14 21:55:38 -0400

    updating movie description and changing email to username on log in screen

--- js/components/MoviesComponent.js
+++ js/components/MoviesComponent.js
@@ -11,7 +11,7 @@ export default {
         
         <div id="lightbox-container">
             <h4 class="media-title">{{mediaDetails.movies_title}}</h4>
-            <p> Sample description sample description sample description sample description</p>
+            <p class="media-desc"> Sample description sample description sample description sample description</p>
         </div>
 
         <div class="image-container">
